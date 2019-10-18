fbcbc572f4dcc73b019adaa2c45b76fec64e3447
Gerben <99052865@mydavinci.nl>
2019-10-18 14:33:07 +0200

    is af maar beter

--- main.js
+++ main.js
@@ -7,11 +7,17 @@ var prijs = 0;
 var aantal = 0;
 var snack = 0;
 
+//start de bestelling.
+function start() {
+	
+//berekent de totaal prijs en laat het zien.
 function _prijs() {
 	prijs = Number(fris) * 1.5 + Number(bier) * 4 + Number(wijn) * 3.5 + Number(snack) * (Number(aantal) * 0.5 );
 	alert("Uw totaalprijs is \u20ac" +prijs+"");
+	drank = "niks"
 }
 
+//bestelt drank
 function _fris() {
 	fris = prompt("Hoeveel " +drank+ " had u gewilt?");
 	drank = "niks";
@@ -27,6 +33,7 @@ function _wijn() {
 	drank = "niks";
 }
 
+//laat de klant bestellen
 while(drank != "stop"){
 	drank = prompt("Welke bestelling wilt u toevoegen? Fris voor \u20ac1,50, bier voor \u20ac4,-, wijn voor \u20ac3,50 of een lekkere snack?");
 	if (drank == "fris") {
@@ -34,30 +41,35 @@ while(drank != "stop"){
 	}
 
 	else if (drank == "bier") {
-		bier();
+		_bier();
 	}
 	
 	else if (drank == "wijn") {
-		wijn();
+		_wijn();
 	}
 
+//laat de klant bitterballen bestellen.
 	else if (drank == "snack") {
 		aantal = prompt("Hoeveel bitterballen wilt u toevoegen (8 voor \u20ac4,- of 16 voor \u20ac8,-)?");
 		if (aantal == 8 || aantal == 16) {
 			snack = prompt("Hoeveel bitterbalschalen van " +aantal+ " stuks wilt u bestellen?");
 		}
+		//als de klant een verkeerd iets invoerd dan krijgt diegene deze error
 		else{ 
 		alert("U kunt alleen een keuze maken tussen 8 en 16. De snacks zijn niet toegevoegd aan de bestelling.");
 		}
 		drank = "niks";
 	}
 
+	//laat je de prijs zien
 	else if (drank == "stop") {
 		_prijs();
 	}
 
+	//error als de klant dom bezig is
 	else if (drank != "niks" || "stop"){
 		alert("U heeft een ongeldige invoer gedaan. Uw bestelling kan niet worden toegevoegd.");
 		drank = "niks";
 	}
+}
 }
